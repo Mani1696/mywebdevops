3fb8a13f847f4654c6155c572351fbc007427225
Mani1696 <manibharathi809@gmail.com>
2023-10-15 22:49:36 +0530

    First commit

---
+++ src/main/java/com/example/MyWebDevops/MyWebDevopsApplication.java
@@ -0,0 +1,13 @@
+package com.example.MyWebDevops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyWebDevopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyWebDevopsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/MyWebDevops/MyWebDevopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MyWebDevops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyWebDevopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
